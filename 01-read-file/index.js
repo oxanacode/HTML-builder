fe5146a47027b1f142f5af4f16a829722587a43c
Oxana <oxanacode@gmail.com>
2022-05-23 13:29:58 +0300

    feat: done 01-read-file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,15 @@
+const {stdout} = process;
+
+const path = require('path');
+const fs = require('fs');
+
+const fileName = path.join(path.dirname(__filename), 'text.txt'); 
+const stream = fs.createReadStream(fileName, 'utf-8');
+
+let data = '';
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => {
+  stdout.write(data);
+  process.exit();
+});
+stream.on('error', error => console.log('Error', error.message));
